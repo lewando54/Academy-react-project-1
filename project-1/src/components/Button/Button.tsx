3dde601c8789a510c53f4ffd071d44d53a448457
Jakub Lewandowski <lewando54@gmail.com>
2023-01-16 18:36:52 +0100

    Button refactor with colors

--- project-1/src/components/Button/Button.tsx
+++ project-1/src/components/Button/Button.tsx
@@ -7,14 +7,16 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 
 function Button({color, children, ...props}: ButtonProps){
     const [bgColor, setBgColor] = useState('');
-
+    const colorOptions: any = {
+        'primary': 'bg-pink-500',
+        'secondary': 'bg-gray-200'
+    }
     useEffect(() => {
-        if(color == 'primary') setBgColor('bg-pink-500');
-        else setBgColor('bg-gray-200');
-    })
+        setBgColor(color);
+    }, [color])
 
     return (
-        <button {...props} className={'rounded-md text-white text-sm w-full py-2 uppercase shadow-[0_4px_6px_rgba(0,0,0,0.3)] tracking-wide '+bgColor}>
+        <button {...props} className={'rounded-md text-white text-sm w-full py-2 uppercase shadow-[0_4px_6px_rgba(0,0,0,0.3)] tracking-wide '+colorOptions[bgColor]}>
             {children}
         </button>
     )
